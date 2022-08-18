9983faa6673e11cf0a4ce2d0e933b5f6297b8547
Tara78 <70331037+Tara78@users.noreply.github.com>
2022-08-18 18:34:40 +0200

    final
    final

---
+++ Salad bar/app.js
@@ -0,0 +1,8 @@
+const detail1= document.querySelector('.detail');
+const btn1= document.querySelector('.btn');
+
+btn1.addEventListener('click',()=>{
+
+  // console.log('hej')
+  detail1.classList.toggle('active');
+});
